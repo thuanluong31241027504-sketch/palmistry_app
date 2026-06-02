d527d06bf70e32206d6437c666b0cd49c9143105
thuanluong31241027504-sketch <thuanluong.31241027504@st.ueh.edu.vn>
2026-06-02 17:54:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -143,7 +143,7 @@ if st.session_state.page == 'home':
     st.markdown("""
     <div class="instruction-container">
         <div class="instruction">
-            > chụp ảnh lòng bàn tay dưới ánh sáng đủ, tay để trong khung hình<span class="static-underscore">_</span><br>
+            > chụp ảnh lòng bàn tay đủ sáng, nằm trong khung hình<span class="static-underscore">_</span><br>
             > nhấn nút app sẽ tự động nhận diện các đường chỉ tay<span class="static-underscore">_</span><br>
             > đọc kết quả giải mã đường sinh mệnh, trí tuệ, tình cảm và vận mệnh<span class="blinking-underscore">_</span>
         </div>
